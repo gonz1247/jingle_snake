5420882d77ea12ab2447829c0a4e7f18f1258601
Gonzo <agonzalez1043@gmail.com>
2025-10-10 19:57:14 -0700

    created snake class that tracks which grid points are part of the snake while it moves and/or grows

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,29 @@ import Header from "./components/Header";
 import GameInputs from "./components/GameInputs";
 import BoardDisplay from "./components/BoardDisplay";
 import Board from "./game_objects/Board";
+import Snake from "./game_objects/Snake";
+
+function MoveSnakeWrapper(row, col, snake, board) {
+  // Three scenarios
+  // 1) Cell is empty to just move snake
+  // 2) Cell has a character so grow snake
+  // 3) Cell is another part of the snake so game is over
+
+  // Do board wrapping at edges
+  row = (row + board.boardSize) % board.boardSize;
+  col = (col + board.boardSize) % board.boardSize;
+  const cell_val = board.cells[row][col];
+  if (cell_val === 1) {
+  } else if (cell_val === 0) {
+    board.updateCellValue(row, col, 1);
+    [row, col] = snake.move_to(row, col);
+    board.restoreCellAvailability(row, col);
+  } else {
+    board.updateCellValue(row, col, 1);
+    snake.grow_at(row, col);
+  }
+  return;
+}
 
 function App() {
   // Game Settings
@@ -11,10 +34,26 @@ function App() {
 
   // Populate board with zeros
   const board = new Board(boardSize);
-  let [row, col] = board.getAvailableCellCoord();
-  board.updateCellValue(row, col, 67);
-  [row, col] = board.getAvailableCellCoord();
+  // Create a snake that starts at center of board
+  let row = Math.floor(boardSize/2)
+  let col = row;
+  const snake = new Snake();
+  snake.grow_at(row, col);
   board.updateCellValue(row, col, 1);
+
+  // Move snake around
+  row = row - 1;
+  MoveSnakeWrapper(row, col, snake, board);
+  row = row - 1;
+  MoveSnakeWrapper(row, col, snake, board);
+  col = col + 1;
+  MoveSnakeWrapper(row, col, snake, board);
+  board.updateCellValue(row, col + 1, 68);
+  MoveSnakeWrapper(row, col + 1, snake, board);
+
+  // Add random letter to board
+  [row, col] = board.getAvailableCellCoord();
+  board.updateCellValue(row, col, 67);
 
   return (
     <>
---
+++ src/game_objects/Snake.js
@@ -0,0 +1,59 @@
+class SnakeNode {
+  // Reverse link list node
+  constructor(row, col) {
+    this.coord = [row, col];
+    this.prev = null;
+  }
+}
+
+class Snake {
+  // Reverse link list to represent snake. Start at tail and move backwards up to head
+  constructor() {
+    this.tail = null;
+    this.head = null;
+    this.length = 0
+  }
+
+  grow_at(row, col) {
+    // Create new node to attach to snake
+    const node = new SnakeNode(row, col);
+    this.length += 1
+    // If no tail then this is first element
+    if (this.tail === null) {
+      this.tail = node;
+      this.head = node;
+      return;
+    }
+    // Attach node to head of snake (leave tail as is)
+    this.head.prev = node;
+    this.head = node;
+  }
+
+  move_to(row, col) {
+    // If no tail then this is first element
+    if (this.tail === null) {
+      // Snake has no nodes so grow instead
+      this.grow_at(row, col);
+      return [null, null];
+    }
+    // Update tail node to have new coordinates
+    const old_coord = this.tail.coord;
+    this.tail.coord = [row, col];
+    // Check if single node snake
+    if (this.tail.prev === null) {
+      // No need to update references since just a single node
+      return old_coord;
+    }
+    // detach tail node
+    const new_tail = this.tail.prev;
+    this.tail.prev = null;
+    // Reattach tail node to head node
+    this.head.prev = this.tail;
+    // Update tail and head poiners
+    this.head = this.tail;
+    this.tail = new_tail;
+    return old_coord;
+  }
+}
+
+export default Snake;
